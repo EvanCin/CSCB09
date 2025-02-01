9060872d923ab36555715ae92503369d7863a990
Evan <111394939+EvanCin@users.noreply.github.com>
2025-02-01 23:58:29 +0000

    Add files via upload

--- testVTM.c
+++ testVTM.c
@@ -2,6 +2,29 @@
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 #include <sys/utsname.h>
+#include <string.h>
+
+int getNumCpus() {
+	FILE* read_file;
+	int num_cpu = 0;
+	char file_path[256] = "/sys/devices/system/cpu/cpu";
+	int path_length = strlen(file_path);
+	//Convert num_cpu to string
+	char str_num_cpu[32];
+	sprintf(str_num_cpu, "%d", num_cpu);
+	strncat(file_path, str_num_cpu, 32);
+	read_file = fopen(file_path, "r");
+	while(read_file != NULL) {
+		printf("Current cpu: %d\n",num_cpu);
+		fclose(read_file);
+		num_cpu++;
+		sprintf(str_num_cpu, "%d", num_cpu);
+		file_path[path_length] = '\0';
+		strncat(file_path, str_num_cpu, 32);
+		read_file = fopen(file_path, "r");
+	}
+	return num_cpu;
+}
 
 int main() {
     printf("HELLO WORLD\n");
@@ -21,29 +44,19 @@ int main() {
     printf("Number of current processes: %d\n", info.procs);
     printf("Available high memory size: %ld\n", info.freehigh);
 
-    FILE* read_file;
-    char line[256];
-    read_file = fopen("/proc/cpuinfo", "r");
-    if(read_file == NULL) {
-	 perror("fopen");
-	 exit(1);
-    }
-    printf("File read successful!\n");
-    while(fgets(line, 256, read_file) != NULL) {
-	    printf("%s\n", line);
-    }
+    //FILE* read_file;
+    //char line[256];
+    //read_file = fopen("/proc/cpuinfo", "r");
+    //if(read_file == NULL) {
+    //     perror("fopen");
+    //     exit(1);
+    //}
+    //printf("File read successful!\n");
+    //while(fgets(line, 256, read_file) != NULL) {
+    //        printf("%s\n", line);
+    //}
+	printf("%d\n", getNumCpus());
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
